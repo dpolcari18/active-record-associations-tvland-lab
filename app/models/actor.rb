78a12109a5772619c7a7f127c973f0edb0a200ca
David Polcari <dpolcari18@gmail.com>
2021-01-26 17:30:19 -0600

    commit tree 1ebf3e72217b392fd8ac061e497030ddbd6c4d48

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
   
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        # binding.pry
+        Character.all.find_all {|character| character.actor_id == self.id}.map {|character| "#{character.name} - #{character.show.name}"}
+    end 
 end
