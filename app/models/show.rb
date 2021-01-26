aae6926fbc1f10ffb4cd266073ba484b0fdced27
David Polcari <dpolcari18@gmail.com>
2021-01-26 17:30:44 -0600

    all tests passing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    Actor.all.map {|actors| actors.full_name}
+  end
 end
